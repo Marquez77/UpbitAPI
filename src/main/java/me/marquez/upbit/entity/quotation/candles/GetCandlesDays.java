5402b67f5026acf39ae99fe72bf851abae41ab41
Marquez77 <62211625+Marquez77@users.noreply.github.com>
2023-11-29 16:03:30 +0900

    Add GetCandlesDays, GetCandlesMinutes

--- src/main/java/me/marquez/upbit/entity/quotation/candles/GetCandlesDays.java
+++ src/main/java/me/marquez/upbit/entity/quotation/candles/GetCandlesDays.java
@@ -2,6 +2,7 @@ package me.marquez.upbit.entity.quotation.candles;
 
 import lombok.Builder;
 import lombok.NonNull;
+import lombok.ToString;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Date;
@@ -18,13 +19,41 @@ public class GetCandlesDays {
     public record Request(
             @NonNull String market,
             @Nullable Date to,
-            @NonNull int count,
+            @Nullable int count,
             @Nullable String convertingPriceUnit
     ) {}
 
+    /**
+     * @param market                    마켓명
+     * @param candle_date_time_utc      캔들 기준 시각(UTC 기준)
+     * @param candle_date_time_kst      캔들 기준 시각(KST 기준)
+     * @param opening_price             시가
+     * @param high_price                고가
+     * @param low_price                 저가
+     * @param trade_price               종가
+     * @param timestamp                 마지막 틱이 저장된 시각
+     * @param candle_acc_trade_price    누적 거래 금액
+     * @param candle_acc_trade_volume   누적 거래량
+     * @param prev_closing_price        전일 종가(UTC 0시 기준)
+     * @param change_price              전일 종가 대비 변화 금액
+     * @param change_rate               전일 종가 대비 변화량
+     * @param converted_trade_price     종가 환산 화폐 단위로 환산된 가격 (요청에 convertingPriceUnit 파라미터 없을 시 해당 필드 포함되지 않음.)
+     */
+    @ToString
     public record Response(
             String market,
-            String candle_date_time_utc,
-            String candle_date_time_kst,
-    )
+            Date candle_date_time_utc,
+            Date candle_date_time_kst,
+            double opening_price,
+            double high_price,
+            double low_price,
+            double trade_price,
+            long timestamp,
+            double candle_acc_trade_price,
+            double candle_acc_trade_volume,
+            double prev_closing_price,
+            double change_price,
+            double change_rate,
+            double converted_trade_price
+    ) {}
 }
