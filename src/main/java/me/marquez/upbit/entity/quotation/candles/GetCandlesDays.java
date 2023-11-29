ae015a79743bcb093870101babf80cbdcbf256ca
Marquez77 <62211625+Marquez77@users.noreply.github.com>
2023-11-29 15:32:07 +0900

    Add GetMarketAll

--- src/main/java/me/marquez/upbit/entity/quotation/candles/GetCandlesDays.java
+++ src/main/java/me/marquez/upbit/entity/quotation/candles/GetCandlesDays.java
@@ -1,4 +1,30 @@
 package me.marquez.upbit.entity.quotation.candles;
 
+import lombok.Builder;
+import lombok.NonNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Date;
+
 public class GetCandlesDays {
+
+    /**
+     * @param market                마켓 코드 (ex. KRW-BTC)
+     * @param to                    마지막 캔들 시각 (exclusive). 비워서 요청시 가장 최근 캔들
+     * @param count                 캔들 개수(최대 200개까지 요청 가능)
+     * @param convertingPriceUnit   종가 환산 화폐 단위 (생략 가능, KRW로 명시할 시 원화 환산 가격을 반환.)
+     */
+    @Builder
+    public record Request(
+            @NonNull String market,
+            @Nullable Date to,
+            @NonNull int count,
+            @Nullable String convertingPriceUnit
+    ) {}
+
+    public record Response(
+            String market,
+            String candle_date_time_utc,
+            String candle_date_time_kst,
+    )
 }
--- src/main/java/me/marquez/upbit/entity/quotation/market/GetMarketAll.java
+++ src/main/java/me/marquez/upbit/entity/quotation/market/GetMarketAll.java
@@ -1,4 +1,51 @@
 package me.marquez.upbit.entity.quotation.market;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.ToString;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * 마켓 코드 조회
+ * 업비트에서 거래 가능한 마켓 목록
+ */
 public class GetMarketAll {
+
+    /**
+     * @param isDetails 유의종목 필드과 같은 상세 정보 노출 여부(선택 파라미터)
+     */
+    @Builder
+    public record Request(
+            @Nullable boolean isDetails
+    ) {}
+
+    /**
+     * @param market            업비트에서 제공중인 시장 정보
+     * @param korean_name       거래 대상 디지털 자산 한글명
+     * @param english_name      거래 대상 디지털 자산 영문명
+     * @param market_warning    유의 종목 여부
+     */
+    @ToString
+    public record Response(
+            @NonNull String market,
+            @NonNull String korean_name,
+            @NonNull String english_name,
+            @NonNull String market_warning
+    ) {
+        @AllArgsConstructor
+        public enum MarketWarning {
+            /**
+             * NONE (해당 사항 없음)
+             */
+            NONE("NONE"),
+
+            /**
+             * CAUTION (투자유의)
+             */
+            CAUTION("CAUTION");
+
+            private final String market_warning;
+        }
+    }
 }
